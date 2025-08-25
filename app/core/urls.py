9e93976e14ec3a9428286204adffc4fda2341f75
Francisco Perez <perez.francisco@hotmail.com>
2025-08-25 14:41:59 -0500

    francpeal

--- app/core/urls.py
+++ app/core/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path("api/suggest/items", suggest_items),
     path("api/suggest/clients", suggest_clients),
 
-    # path("api/items/<str:codigo>", item_detail),
-    # path("api/clients/<str:cliente_id>", client_detail),
+    path("api/items/<str:codigo>", item_detail),
+    path("api/clients/<str:cliente_id>", client_detail),
 ]
